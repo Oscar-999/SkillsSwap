c3fb20399069fed5c89109f94afc2d30b8b09843
oscar <oscarvac74@gmail.com>
2023-08-12 16:51:52 -0500

    request routes adder

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,5 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .updateskill_form import UpdateSkillForm
 from .createskill_form import CreateSkillForm
+from .createrequest_form import CreateRequestForm
+from .updaterequest_form import UpdateRequestForm
