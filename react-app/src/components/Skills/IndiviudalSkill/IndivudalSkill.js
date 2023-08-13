b0a01ba69fd9c9da552895896d24bc47b273f1b5
oscar <oscarvac74@gmail.com>
2023-08-13 18:44:00 -0500

    s

--- react-app/src/components/Skills/IndiviudalSkill/IndivudalSkill.js
+++ react-app/src/components/Skills/IndiviudalSkill/IndivudalSkill.js
@@ -2,63 +2,72 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadSingleSkillThunk } from '../../../store/skill';
-import './IndivudalSkill.css'
+import { fetchReviews } from '../../../store/review';
+import './IndivudalSkill.css';
 
 import OpenModalButton from '../../OpenModalButton';
 import SkillManger from '../Manager';
+
 const SingleSkill = () => {
   const { skillId } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadSingleSkillThunk(skillId));
+    dispatch(fetchReviews(skillId)); // Fetch reviews
   }, [dispatch, skillId]);
 
-  const skill = useSelector(state => state.skills.singleSkill); // Change variable name to 'skill'
-  const user = useSelector((state) => state.session.user);
+  const skill = useSelector(state => state.skills.singleSkill);
+  const user = useSelector(state => state.session.user);
+  const reviews = useSelector(state => state.reviews.skill.reviews); // Access nested reviews
 
   const userId = user.id;
   const isOwner = userId === skill.ownerId;
-  
-    console.log("userId:", userId);
-    console.log("skill.ownerId:", skill.ownerId);
-    console.log("isOwner:", isOwner);
-  if (!skill) { // Change variable name here
+
+  if (!skill) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      {skill ? (
-        <div className="friendsz">
-          <h1 className="server-name">
-            {skill.name}
-            {isOwner ? " " : null}
-            {isOwner ? (
-              <OpenModalButton
-                modalComponent={<SkillManger skill={skill} />}
-                buttonText="&#x2699;"
-                className={"server-emoji-button"}
-              />
-            ) : null}
-          </h1>
-          <img
-            className="singleimgban"
-            src={skill.skillImage}
-            alt="Banner Image"
-          />
-          <img
-            className="singleimg"
-            src={skill.secondaryImage}
-            alt="Server Image"
-          />
-          <p className="server-name">{skill.description}</p>
-        </div>
-      ) : (
-        <p>Loading server information...</p>
-      )}
+      <div className="friendsz">
+        <h1 className="server-name">
+          {skill.name}
+          {isOwner ? " " : null}
+          {isOwner ? (
+            <OpenModalButton
+              modalComponent={<SkillManger skill={skill} />}
+              buttonText="&#x2699;"
+              className={"server-emoji-button"}
+            />
+          ) : null}
+        </h1>
+        <img
+          className="singleimgban"
+          src={skill.skillImage}
+          alt="Banner Image"
+        />
+        <img
+          className="singleimg"
+          src={skill.secondaryImage}
+          alt="Server Image"
+        />
+        <p className="server-name">{skill.description}</p>
+      </div>
+
+      <div className="reviews-container">
+        <h2>Reviews:</h2>
+        {reviews.map(review => (
+          <div key={review.id} className="review-item">
+            <p>{review.reviewText}</p>
+            <p>Reviewer ID: {review.reviewerId}</p>
+            <p>Created At: {review.createdAt}</p>
+            {/* Display other review properties */}
+          </div>
+        ))}
+      </div>
     </div>
   );
-      }
+};
 
 export default SingleSkill;
--- react-app/src/components/Skills/Manager/index.js
+++ react-app/src/components/Skills/Manager/index.js
@@ -1,13 +1,16 @@
 import OpenModalButton from "../../OpenModalButton"
 import DeleteMulti from "./Delete/Delete"
 import SkillUpdateModal from './Update/SkillUpdateModal'
+import './SkillManager.css'
 const SkillManger = ({skill}) => {
 return (
-    <div>
+    <div className="modal-wrap-manage">
         <h1 className="manager-head">Manage {skill.name}</h1>
         <h2>Manage This Skill:</h2>
-        <OpenModalButton className="skill-button" buttonText='Update Skill' modalComponent={<SkillUpdateModal title='Update Skill' skill={skill} />} />
-        <OpenModalButton className="skill-button" id="delete" buttonText="Delete Skill"  modalComponent={<DeleteMulti type="skill" id={skill.id} />} />
+        <div className="modal-sec">
+        <OpenModalButton className="skill-buttn" id='update' buttonText='Update Skill' modalComponent={<SkillUpdateModal title='Update Skill' skill={skill} />} />
+        <OpenModalButton className="skill-buttn" id="delete" buttonText="Delete Skill"  modalComponent={<DeleteMulti type="skill" id={skill.id} />} />
+        </div>
     </div>
 )
 }
