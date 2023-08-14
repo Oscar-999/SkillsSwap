bdb416479edef31c4a925ae9dc276dd07c51d66e
oscar <oscarvac74@gmail.com>
2023-08-13 23:48:06 -0500

    all

--- react-app/src/components/Skills/Manager/Create/CreateSkillModal.js
+++ react-app/src/components/Skills/Manager/Create/CreateSkillModal.js
@@ -1,9 +1,10 @@
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 import { useModal } from "../../../../context/Modal";
-import { useEffect, useState } from "react";
 import { createSkillThunk } from "../../../../store/skill";
-import "./CreateSkillModal.css"
+import "./CreateSkillModal.css";
+
 const CreateSkillModal = () => {
   const dispatch = useDispatch();
   const history = useHistory();
@@ -14,9 +15,9 @@ const CreateSkillModal = () => {
   const [skillImage, setSkillImage] = useState(null);
   const [secondaryImage, setSecondaryImage] = useState(null);
   const [thirdImage, setThirdImage] = useState(null);
-  // eslint-disable-next-line no-unused-vars
-  const [error, setError] = useState({});
+  const [error, setError] = useState([]);
   const [disableButton, setDisableButton] = useState(true);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newErrors = [];
@@ -24,9 +25,11 @@ const CreateSkillModal = () => {
     if (!name.length || name.length > 255)
       newErrors.push("Name must be between 1 and 255 characters");
     if (!description.length || description.length > 255)
-      newErrors.push("Name must be between 1 and 255 characters");
+      newErrors.push("Description must be between 1 and 255 characters");
     if (!skillImage || !secondaryImage || !thirdImage)
       newErrors.push("Please add all three images");
+    if (price < 1)
+      newErrors.push("Price must be at least 1");
 
     if (newErrors.length) {
       setError(newErrors);
@@ -41,6 +44,7 @@ const CreateSkillModal = () => {
     form.append("skill_image", skillImage);
     form.append("secondary_image", secondaryImage);
     form.append("third_image", thirdImage);
+
     dispatch(createSkillThunk(form)).then((responseData) => {
       if (responseData.error) {
         setError(responseData.error);
@@ -60,11 +64,14 @@ const CreateSkillModal = () => {
       newErrors.push("Description must be between 1 and 255 characters");
     if (!skillImage || !secondaryImage || !thirdImage)
       newErrors.push("Please add all three images");
+    if (price < 1)
+      newErrors.push("Price must be at least 1");
 
     if (newErrors.length) setDisableButton(true);
-  }, [name, description, secondaryImage,skillImage,thirdImage]);
+  }, [name, description, secondaryImage, skillImage, thirdImage, price]);
 
   return (
+    <div className="skillcreateback">
     <div className="create-wrapper">
       <h1>Create a Skill</h1>
       {error.length
@@ -72,34 +79,31 @@ const CreateSkillModal = () => {
         : null}
 
       <form className="form-box" onSubmit={handleSubmit} encType="multipart/form-data">
-        <label htmlFor="skill-create-name">Skill Name</label>
+        <label htmlFor="skill-create-name">Skill Name <i style={{ color: 'red' }}>*</i></label>
         <input
-          id=""
           type="text"
           value={name}
           required
           onChange={(e) => setName(e.target.value)}
           placeholder="What would you like to call this skill?"
         />
-        <label htmlFor="skill-create-price">Skill Price</label>
+        <label htmlFor="skill-create-price">Skill Price <i style={{ color: 'red' }}>*</i></label>
         <input
-          id=""
           type="number"
           value={price}
           required
           onChange={(e) => setPrice(e.target.value)}
           placeholder="Set a price for your skill"
         />
-        <label htmlFor="skill-create-description">Skill Description</label>
+        <label htmlFor="skill-create-description">Skill Description <i style={{ color: 'red' }}>*</i></label>
         <input
-          id=""
           type="text"
           value={description}
           required
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Describe your skill to your customers"
         />
-        <label htmlFor="skill-create-image">New Skill Image</label>
+        <label htmlFor="skill-create-image">New Skill Image <i style={{ color: 'red' }}>*</i></label>
         <input
           type="file"
           name="skill-form-skill-image"
@@ -107,7 +111,7 @@ const CreateSkillModal = () => {
           onChange={(e) => setSkillImage(e.target.files[0])}
           accept="image/*"
         />
-        <label htmlFor="secondary-create-image">New Secondary Image</label>
+        <label htmlFor="secondary-create-image">New Secondary Image <i style={{ color: 'red' }}>*</i></label>
         <input
           type="file"
           name="skill-form-secondary-image"
@@ -115,8 +119,7 @@ const CreateSkillModal = () => {
           onChange={(e) => setSecondaryImage(e.target.files[0])}
           accept="image/*"
         />
-        <label htmlFor="third-create-image">New Third Image</label>
-
+        <label htmlFor="third-create-image">New Third Image <i style={{ color: 'red' }}>*</i></label>
         <input
           type="file"
           name="skill-form-third-image"
@@ -133,6 +136,7 @@ const CreateSkillModal = () => {
           Create Skill
         </button>
       </form>
+    </div>
     </div>
   );
 };
